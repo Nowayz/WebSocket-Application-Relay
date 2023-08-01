aba706b218be44787481997b264d6b269deefcfe
Mike Orozco <me@mikeorozco.dev>
2023-08-01 09:19:27 -0500

    Update type definitions * Remove re.OP definition, replace with [key: string]: unknown; * Add GenericObject and GenericFunction types * Improve typings for Binary and Text message handlers * Replace `keyof re.OP` with `number` type

--- relay.d.ts
+++ relay.d.ts
@@ -1,19 +1,19 @@
+type GenericObject = Record<string, unknown>;
+type GenericFunction = (...args: unknown[]) => unknown;
+
 export declare const re: {
   BINARY_BROADCAST: Uint8Array,
   RELAY_QUERY: Uint8Array,
   TEXT_BROADCAST: string,
   BINARY: number,
   TEXT: number,
-  OP: {
-    ANNOUNCE: number,
-    ANNOUNCE_REPLY: number,
-  },
-  USERS: {},
+  USERS: {}, 
   UInt8UserIdToBase64: (userId: Uint8Array) => string,
   Base64ToUInt8UserID: (userId: string) => Uint8Array,
   ArrayToStr: (x: array) => string,
   StrToArray: (x: string) => array,
   UserIdHex: (userId: Uint8Array) => string,
+  [key: string]: unknown,
 };
 
 export declare const ArrayEq: (a: array, b: array) => boolean;
@@ -22,22 +22,22 @@ export declare const Base64ToUInt8UserID: (userId: string) => Uint8Array;
 
 export declare class Relay {
   public ws: WebSocket;
-  public onClientDisconnect: ((userId: Uint8Array) => void) | null;
+  public onClientDisconnect: ((userId: Uint8Array) => unknown) | null;
   public onSubscription: ((userId: Uint8Array) => Relay) | null;
-  public BinaryMessageHandlers: { [key: keyof re.OP]: (sender: Uint8Array, message: Uint8Array | object) => void };
-  public TextMessageHandlers: { [key: keyof re.OP]: (sender: Uint8Array, message: Uint8Array | object) => void };
-  public VariableCallbacks: Function[];
+  public BinaryMessageHandlers: { [key: number]: (sender: Uint8Array, message: Uint8Array) => unknown };
+  public TextMessageHandlers: { [key: number]: (sender: Uint8Array, message: GenericObject) => unknown };
+  public VariableCallbacks: GenericFunction[];
   public channelName: string;
   public ready: boolean;
   public userId: Uint8Array;
 
   constructor(relayURL: string, channelName: string);
   public JoinChannel(): void;
-  public SetMessageHandler(msgType: number, OpCode: keyof re.OP, callback: unknown): void;
+  public SetMessageHandler(msgType: number, OpCode: number, callback: unknown): void;
   public MessageDispatcher(e: MessageEvent): void;
   public FirstMessageHandler(e: MessageEvent): void;
-  public bSendTo(target: Uint8Array, OpCode: keyof re.OP, msg: any): void;
+  public bSendTo(target: Uint8Array, OpCode: number, msg: any): void;
   public tSendTo(target: string | Uint8Array, obj: any): void;
   public SetChannelVar(key: string, value: any): void;
-  public GetChannelVar(key: string, callback: Function): void;
+  public GetChannelVar(key: string, callback: GenericFunction): void;
 }
--- relay.module.d.ts
+++ relay.module.d.ts
@@ -1,12 +1,10 @@
+type GenericObject = Record<string, unknown>;
+type GenericFunction = (...args: unknown[]) => unknown;
+
 export declare const QUERY: Uint8Array;
 export declare const BROADCAST: Uint8Array;
 export declare const BINARY_TYPE: number;
 export declare const TEXT_TYPE: number;
-
-export declare const OP: {
-  ANNOUNCE: number,
-  ANNOUNCE_REPLY: number,
-}
 
 export declare const ArrayEq: (a: array, b: array) => boolean;
 export declare const UInt8UserIdToBase64: (userId: Uint8Array) => string;
@@ -17,22 +15,22 @@ export declare const UserIdHex: (userId: Uint8Array) => string;
 
 export declare class Relay {
   public ws: WebSocket;
-  public onClientDisconnect: ((userId: Uint8Array) => void) | null;
+  public onClientDisconnect: ((userId: Uint8Array) => unknown) | null;
   public onSubscription: ((userId: Uint8Array) => Relay) | null;
-  public BinaryMessageHandlers: { [key: keyof re.OP]: (sender: Uint8Array, message: Uint8Array | object) => void };
-  public TextMessageHandlers: { [key: keyof re.OP]: (sender: Uint8Array, message: Uint8Array | object) => void };
-  public VariableCallbacks: Function[];
+  public BinaryMessageHandlers: { [key: number]: (sender: Uint8Array, message: Uint8Array) => unknown };
+  public TextMessageHandlers: { [key: number]: (sender: Uint8Array, message: GenericObject) => unknown };
+  public VariableCallbacks: GenericFunction[];
   public channelName: string;
   public ready: boolean;
   public userId: Uint8Array;
 
   constructor(relayURL: string, channelName: string);
   public JoinChannel(): void;
-  public SetMessageHandler(msgType: number, OpCode: keyof re.OP, callback: unknown): void;
+  public SetMessageHandler(msgType: number, OpCode: number, callback: unknown): void;
   public MessageDispatcher(e: MessageEvent): void;
   public FirstMessageHandler(e: MessageEvent): void;
-  public bSendTo(target: Uint8Array, OpCode: keyof re.OP, msg: any): void;
+  public bSendTo(target: Uint8Array, OpCode: number, msg: any): void;
   public tSendTo(target: string | Uint8Array, obj: any): void;
   public SetChannelVar(key: string, value: any): void;
-  public GetChannelVar(key: string, callback: Function): void;
+  public GetChannelVar(key: string, callback: GenericFunction): void;
 }
